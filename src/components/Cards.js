48fa2158b8f20a535addc744cf315fd0c679b3d9
RunaBirkeland <runa.birkeland@gmail.com>
2023-07-26 10:40:27 +0200

    Redesigned Cards component with self made graphics

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,33 +1,41 @@
 import React from "react"
 import "./Cards.css"
 import CardItem from "./CardItem"
-import CodeImage from "./images/code_square.png"
-import UiImage from "./images/hear_1.png"
-import DesignImage from "./images/poster.png"
+import WebDev from "./images/web_dev.png"
+import WebDes from "./images/web_design.png"
+import GraphDes from "./images/graphic_design.png"
+import UserRes from "./images/user_research.png"
 
 function Cards() {
     return (
         <div className="cards">
-            <h1>This is a test</h1>
             <div className="cards_container">
                 <div className="cards_wrapper">
                     <ul className="cards_items">
                         <CardItem
-                            src={ CodeImage }
-                            text="Coding"
-                            alt="Coding"
+                            src={ WebDev }
+                            text="Front-end development"
+                            alt="Coding Icon"
                             path="/projects"
                         />
                         <CardItem
-                            src={ UiImage }
-                            text="UX/UI"
-                            alt="UI Design"
+                            src={ WebDes }
+                            text="Mobile and Web application design"
+                            alt="UI Design Icon"
+                            path="/projects"
+                        />
+                        </ul>
+                        <ul className="cards_items">
+                        <CardItem
+                            src={ GraphDes }
+                            text="Print and Graphic Design"
+                            alt="Graphic Design Icon"
                             path="/projects"
                         />
                         <CardItem
-                            src={ DesignImage }
-                            text="Graphic Design"
-                            alt="Graphic Design"
+                            src={ UserRes }
+                            text="User Research"
+                            alt="User Research Icon"
                             path="/projects"
                         />
                     </ul>
