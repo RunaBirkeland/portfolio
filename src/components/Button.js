32f0963574f1b7042667a7feeaecf158d809dc0e
RunaBirkeland <runa.birkeland@gmail.com>
2023-07-26 20:52:42 +0200

    Cleaned up the content in the Projects page, and fixed image styling so everything is in line. Added code to Button component so the project links open in new tab.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,12 @@ import React from "react"
 import "./Button.css"
 import { Link } from "react-router-dom"
 
-function Button (props, type, onClick) {
+function Button(props, type, onClick) {
     return (
-        <Link to={props.path} className="btn-mobile">
+        <Link to={props.path} target="_blank" className="btn-mobile">
             <button className="btn"
-            onClick={onClick}
-            type={type}>
+                onClick={onClick}
+                type={type}>
                 {props.title}
             </button>
         </Link>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,38 +5,37 @@ import Button from "./Button"
 import Logo from "./images/monogram_coal.png"
 import Greentrotter from "./images/greentrotter_logo.png"
 import Hermes from "./images/hermes.png"
-import Bearth from "./images/b_earth.png"
 import ChargeBnB from "./images/chargebnb.png"
 import Hear from "./images/logo_hear.png"
+import HeroSection from "./HeroSection"
 
 function Posts() {
     return (
         <div className="posts">
+            <HeroSection
+                text="A selection of websites and prototypes. Designed and coded by me."
+            />
             <div className="post-container">
-                <h1 className="category-header">PORTFOLIO</h1>
-                <p className="category-introduction">The below is a list of selected work. The aim of this list is to show growth and development over time, as well as future potential.</p>
                 <ul className="post-items">
                     <li className="post-item">
                         <div className="post-item-wrapper">
                             <PostItem
-                                alt="Logo"
-                                src={Logo}
-                                header="Portfolio Website"
-                                text="I created this website in order to provide myself 
-                                with an easily accessible and professional portfolio, 
-                                it also gave me the opportunity to teach myself React JS. 
-                                As this website will be a constant work-in-progress 
-                                it is subject to continual change and development, 
-                                both for the benfit of the users as well as for my own 
-                                personal development. Apart from the icons used for the responsive 
-                                menu and the Footer, all the graphics and icons are 
-                                made by yours truly. 
-                                The website is developed using and hosted by GitHub, 
-                                so the button below will lead to the GitHub project."
+                                alt="(H)ear Logo"
+                                src={Hear}
+                                header="(H)ear Redesign"
+                                text="This is a prototype which was created as part of my bachelor thesis.
+                                During the project my partner and I collaborated with a start-up called Woid AS,
+                                a company which is developing an assistive technology application. The project
+                                involved a comprehensive redesign of the application (h)ear. 
+                                The color palette, icon design, illustrations, and 
+                                walkthrough were solely developed by me, while the remaining 
+                                aspects were the result of collaborative efforts with my 
+                                partner, where we both contributed equally to the overall 
+                                design. The button leads to the Figma file."
                             />
                             <div className="button-wrap">
                                 <Button
-                                    path="/"
+                                    path="https://www.figma.com/proto/MnFtHJYshlSGM9d9IyTS0O/(h)ear-suggestions-for-new-prototype?node-i d=94-1800&scaling=scale-down&page-id=0%3A1&starting-point-node-id=94%3A1800200.github.io/Final_Exam/index.html"
                                     title="View Project"
                                 />
                             </div>
@@ -69,22 +68,24 @@ function Posts() {
                     <li className="post-item">
                         <div className="post-item-wrapper">
                             <PostItem
-                                alt="(H)ear Logo"
-                                src={Hear}
-                                header="(H)ear Redesign"
-                                text="This is a prototype which was created as part of my bachelor thesis.
-                                During the project my partner and I collaborated with a start-up called Woid AS,
-                                a company which is developing an assistive technology application. The project
-                                involved a comprehensive redesign of the application (h)ear. 
-                                The color palette, icon design, illustrations, and 
-                                walkthrough were solely developed by me, while the remaining 
-                                aspects were the result of collaborative efforts with my 
-                                partner, where we both contributed equally to the overall 
-                                design. The button leads to the Figma file."
+                                alt="Logo"
+                                src={Logo}
+                                header="Portfolio Website"
+                                text="I created this website in order to provide myself 
+                                with an easily accessible and professional portfolio, 
+                                it also gave me the opportunity to teach myself React JS. 
+                                As this website will be a constant work-in-progress 
+                                it is subject to continual change and development, 
+                                both for the benfit of the users as well as for my own 
+                                personal development. Apart from the icons used for the responsive 
+                                menu and the Footer, all the graphics and icons are 
+                                made by yours truly. 
+                                The website is developed using and hosted by GitHub, 
+                                so the button below will lead to the GitHub project."
                             />
                             <div className="button-wrap">
                                 <Button
-                                    path="https://www.figma.com/proto/MnFtHJYshlSGM9d9IyTS0O/(h)ear-suggestions-for-new-prototype?node-i d=94-1800&scaling=scale-down&page-id=0%3A1&starting-point-node-id=94%3A1800200.github.io/Final_Exam/index.html"
+                                    path="/"
                                     title="View Project"
                                 />
                             </div>
@@ -114,28 +115,6 @@ function Posts() {
                     <li className="post-item">
                         <div className="post-item-wrapper">
                             <PostItem
-                                alt="B.Earth Logo"
-                                src={Bearth}
-                                header="B.earth Website"
-                                text="This is a prototype for a website which was developed 
-                                during the course of Prototyping and Interaction Design in the 
-                                autumn of 2020. The website prototype was an integral part of a 
-                                larger exam assignment. The intention was to showcase a fictional 
-                                company selling a physical product, which was also prototyped. 
-                                As the main driving force, I was responsible for both the digital
-                                and the physical prototypes."
-                            />
-                            <div className="button-wrap">
-                                <Button
-                                    path="https://xd.adobe.com/view/46ca5fac-add4-4749-845c-f0955145e312-1399/?fullscreen"
-                                    title="View Project"
-                                />
-                            </div>
-                        </div>
-                    </li>
-                    <li className="post-item">
-                        <div className="post-item-wrapper">
-                            <PostItem
                                 alt="Hermes logo"
                                 src={Hermes}
                                 header="Hermes Mobile Application"
@@ -148,7 +127,7 @@ function Posts() {
                             />
                             <div className="button-wrap">
                                 <Button
-                                    path="https://xd.adobe.com/view/46ca5fac-add4-4749-845c-f0955145e312-1399/?fullscreen"
+                                    path="https://xd.adobe.com/view/005c8c3c-923c-4a85-b33a-493167a9249d-550f/"
                                     title="View Project"
                                 />
                             </div>
