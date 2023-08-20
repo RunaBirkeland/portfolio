c43ef5bf72e7063964c562e64fdf5d0bdfaf430c
RunaBirkeland <runa.birkeland@gmail.com>
2023-08-20 13:59:06 +0200

    Added more content

--- src/components/resumee/EducationList.js
+++ src/components/resumee/EducationList.js
@@ -10,13 +10,18 @@ function EducationList() {
                 <ul className="list">
                     <ListItem
                         header="Applied Computer Science"
-                        date="2019 - 2023"
-                        description="Bachelor degree with a specialization in HCI."
+                        date="2020 - 2023"
+                        description="Bachelor degree with a specialization in both UX/UI design and Frontend development. Graduated with an average grade of A."
                     />
                     <ListItem
                         header="MA Acting"
                         date="2017 - 2018"
-                        description="A Master of the Arts degree in Acting."
+                        description="Master of the Arts degree in acting."
+                    />
+                    <ListItem
+                        header="BA Musical Theatre"
+                        date="2013 - 2017"
+                        description="Bachelor of the Arts degree in acting, dancing and singing."
                     />
                 </ul>
             </div>
--- src/components/resumee/ExperienceList.js
+++ src/components/resumee/ExperienceList.js
@@ -10,12 +10,22 @@ function ExperienceList () {
                 <ListItem 
                     header="Teacher's Assistant"
                     date="2021 - 2023"
-                    description="TA in subjects like Prototyping and Interaction Design, Visualization and Web Development."
+                    description="TA in the subjects Interaction Design and Prototyping, Web Development, and Visualization. Planning and conducting lectures, assisting the lecturer with classes and grading assignments, as well as guiding and assisting students."
                 />
                 <ListItem 
-                    header="Intern"
+                    header="UX/UI Intern"
                     date="2023"
-                    description="A short-term internship, as part of the bachelor thesis, with a newly founded start-up developing assistive technology."
+                    description="Internship at a startup in connection with the bachelor’s thesis. Conducted user testing on a mobile app and redesigned the application based on the collected data."
+                />
+                <ListItem 
+                    header="UX Intern"
+                    date="2022"
+                    description="Internship at a startup. Created and tested a prototype of a desktop app for managing electric vehicle chargers."
+                />
+                <ListItem 
+                    header="Various"
+                    date=""
+                    description="I've also produced, starred in and help set up multiple theatre productions and short films, worked as a bar manager in London, UK, and worked in the charitable organization 'Future in our Hands'."
                 />
             </ul>
         </div>
